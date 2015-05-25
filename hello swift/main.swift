3d4421b61e089302b4296be16384aa7120c81ea8
kenji <kenji256@gmail.com>
2015-05-25 19:23:00 +0900

    Initial Commit

---
+++ hello swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello swift
+//
+//  Created by kenji on 2015/05/25.
+//  Copyright (c) 2015年 kenji. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
